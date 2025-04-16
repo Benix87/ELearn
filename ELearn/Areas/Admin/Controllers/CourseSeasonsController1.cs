24bbd99999bd38471486c7a17642bfc3620a6ecb
Fanni-PC-4 <Fanni-PC-4@FANNI-PC-4>
2025-04-16 12:34:18 +0330

    Add Controllers

---
+++ ELearn/Areas/Admin/Controllers/CourseSeasonsController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELearn.Areas.Admin.Controllers
+{
+    public class CourseSeasonsController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
