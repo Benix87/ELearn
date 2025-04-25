b51ea91924dc74f5ccd6ad2cf43315d1fd2de886
Benyamin Faraji <bnyamynfrjy008@gmail.com>
2025-04-25 22:52:46 +0330

    Update CoursesController.cs

--- ELearn/Areas/Admin/Controllers/CoursesController.cs
+++ ELearn/Areas/Admin/Controllers/CoursesController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ELearn.Core.Interfaces;
+using ELearn.DataLayer.DTOS;
+using ELearn.DataLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ELearn.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CoursesController : Controller
     {
-        public IActionResult Index()
+        private readonly ICourse _CourseServices;
+        public CoursesController(ICourse CourseServices)
+        {
+            _CourseServices = CourseServices;
+        }
+        public  async Task<IActionResult> Index()
+        {
+            List<Course> courses  = await _CourseServices.GetCourses();
+            return View(courses);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCourse()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCourse(CreateCourseViewModel createCourseViewModel)
+        {
+            return Redirect("/Admin/Home");
         }
     }
 }
