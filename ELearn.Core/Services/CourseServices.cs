4c3f1aeb8f836ea0898299969a81731ef834542a
Benyamin Faraji <bnyamynfrjy008@gmail.com>
2025-04-25 22:44:30 +0330

    Update CourseServices.cs

--- ELearn.Core/Services/CourseServices.cs
+++ ELearn.Core/Services/CourseServices.cs
@@ -1,11 +1,12 @@
-﻿using ELearn.Core.Interfaces;
-using ELearn.DataLayer.Context;
-using ELearn.DataLayer.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ELearn.Core.Interfaces;
+using ELearn.DataLayer.Context;
+using ELearn.DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ELearn.Core.Services
 {
@@ -16,69 +17,16 @@ namespace ELearn.Core.Services
         {
             _context = context;
         }
-        public async Task<bool> CreateCourse(Course cource)
+
+
+        public async Task<int> GetCourseCounts()
         {
-            try
-            {
-                await _context.Course.AddAsync(cource);
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return false;
-            }
+            return await _context.Courses.CountAsync();   
         }
 
-        public async Task<bool> DeleteCourse(int id)
+        public async Task<List<Course>> GetCourses()
         {
-            var course = await GetCourseById(id);
-            if (course == null)
-            {
-                throw new Exception("This Course Group Not Found");
-            }
-            try
-            {
-                course.IsDeleted = true;
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return false;
-            }
-        }
-
-        public async Task<Course> GetCourseById(int id)
-        {
-            var course = await _context.Course.FindAsync(id);
-            if (course == null)
-            {
-                throw new Exception("This Course Group Not Found");
-            }
-            return course;
-        }
-
-        public IEnumerable<Course> GetCourses()
-        {
-            return _context.Course;
-        }
-
-        public async Task<bool> UpdateCourse(Course course)
-        {
-            try
-            {
-                _context.Course.Update(course);
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return false;
-            }
+           return await _context.Courses.ToListAsync();
         }
     }
 }
