519145c3df40ab03076996ce17a2779e5780ee54
Benyamin Faraji <bnyamynfrjy008@gmail.com>
2025-04-25 22:54:14 +0330

    Update HomeController.cs

--- ELearn/Areas/Admin/Controllers/HomeController.cs
+++ ELearn/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ELearn.Core.Interfaces;
+using ELearn.DataLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ELearn.Areas.Admin.Controllers
 {
-    [Area("Admin")] 
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        //private readonly IUser _UserServices;
+        private readonly ICourse _CourseServices;
+        private readonly ICourseGroup _CourseGroupServices;
+        public HomeController(ICourse CourseServices, ICourseGroup CourseGroupServices)
         {
+            _CourseGroupServices = CourseGroupServices;
+            _CourseServices = CourseServices;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.CourseCounts =await _CourseServices.GetCourseCounts();
+            ViewBag.CourseGroupCounts = await _CourseGroupServices.GetCourseGroupCounts();
             return View();
         }
     }
-}
