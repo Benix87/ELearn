3f489636845343b1531d645bfdbeea4c0717159a
Benyamin Faraji <bnyamynfrjy008@gmail.com>
2025-04-25 22:37:46 +0330

    Update ICourse.cs

--- ELearn.Core/Interfaces/ICourse.cs
+++ ELearn.Core/Interfaces/ICourse.cs
@@ -1,7 +1,9 @@
+﻿using ELearn.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ELearn.DataLayer.Entities;
 
 namespace ELearn.Core.Interfaces
 {
