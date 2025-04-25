e4bcfdec0cf31f1a80b51481516ad9248eaa6d5a
Benyamin Faraji <bnyamynfrjy008@gmail.com>
2025-04-25 22:47:38 +0330

    Update CreateCourseGroupViewModel.cs

--- ELearn.DataLayer/DTOS/CourseGroupViewModels/CreateCourseGroupViewModel.cs
+++ ELearn.DataLayer/DTOS/CourseGroupViewModels/CreateCourseGroupViewModel.cs
@@ -1,17 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
-namespace ELearn.DataLayer.DTOS.CourseGroupViewModels
+namespace ELearn.DataLayer.DTOS
 {
-    public class CreateCourseGroupViewModel
+    
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+
+    public class CreateCourseViewModel
     {
-        [Required(ErrorMessage = "مقدار نام گروه نمیتواند خالی باشد")]
-        [Display(Name = "نام گروه")]
-        [MaxLength(15, ErrorMessage = "مقدار کاراکتر ها نهایتا باید 15 عدد باشد")]
-        public string CourseGroupName { get; set; }
+        [Required, StringLength(100)]
+        [Display(Name = "Course Title")]
+        public string CourseTitle { get; set; }
+
+        [Required]
+        [Display(Name = "Thumbnail")]
+        public IFormFile CourseThumbnail { get; set; }
+
+        [Display(Name = "Free Course?")]
+        public bool IsFree { get; set; } = true;
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "Please enter a valid price")]
+        [Display(Name = "Price (if not free)")]
+        public double? Price { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string CourseDescription { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        [Display(Name = "Publish Date")]
+        public DateTimeOffset PublishDate { get; set; }
+
+        [Display(Name = "Published?")]
+        public bool IsPublished { get; set; } = true;
     }
+
 }
